8c6dd61df72d261658339b684b4009b1c7f3e015
Leandro Mariperisena <leandro.mariperisena@gmail.com>
2021-07-26 21:14:52 -0300

    Se añadiò la operatoria con el Localstorage para guardar el carrito y renderizarlo al ingresar a la pàgina. Se añadiò la renderizaciòn de los productos del arreglo de productos al acceder a la pàgina. Se agregaron correcciones y mejoras visuales en la pàgina en general

--- js/main.js
+++ js/main.js
@@ -1,29 +1,32 @@
+//------------ Variables y selectores ------------ //
+
+
 const products = [
     {
         id: "1",
-        name: "La metamorfosis",
+        name: "The metamorphosis",
         author: "Franz Kafka",
         price: 1300,
         stock: 10
     },
     {
         id: "2",
-        name: "Crónica de una muerte anunciada",
-        author: "Gabriel García Márquez",
+        name: "Pride and Prejudice",
+        author: "Jane Austen",
         price: 1800,
         stock: 0
     },
     {
         id: "3",
-        name: "El señor de los Anillos: La Comunidad del Anillo",
-        author: "J.R.R Tolkien",
+        name: "Moby-Dick",
+        author: "Herman Melville",
         price: 2300,
         stock: 20
     },
     {
         id: "4",
-        name: "El Arnoldo: podcast",
-        author: "El arnoldo",
+        name: "River God",
+        author: "Wilbur Smith",
         price: 1000,
         stock: 3
     },
@@ -41,10 +44,33 @@ const products = [
         price: 1800,
         stock: 22
     },
-];
-const cart = [];
+]; // Arreglo con los productos disponibles
+
+const cart = []; //Carrito de compras
 
 const iva = x => x * 0.21; //Calcula directamente el IVA de un producto
+
+const saveLocal = (key, value) => { localStorage.setItem(key, value) }; //Función para añadir en Localstorage
+
+const btnBuyAll = document.querySelector(".btn-buyall"); //Botón final de compra
+
+const btnEmptyCart = document.querySelector(".btn-emptycart"); //Botón para vaciar el carrito
+
+const contactForm = $(".contact-form"); //Formulario de contacto seleccionado con jQuery
+
+
+//------------ Listeners ------------ //
+
+
+btnEmptyCart.addEventListener('click', emptyCart); //Evento para botón de vaciar el carrito
+
+btnBuyAll.addEventListener('click', informTotal); //Evento para informar el costo total y vaciar carrito
+
+contactForm.submit(validateForm); //Evento para validar formulario y enviar datos mediante AJAX
+
+
+// ------------ Funciones ------------ //
+
 
 //Función que calcula el precio del producto con el IVA incluído
 
@@ -52,17 +78,12 @@ function calculatePriceIva(mount) {
     return mount + iva(mount);
 }
 
-//Función para eliminar un producto del carrito cuyo ID se recibe como parámetro
-
-function deleteProduct(id) {
-    cart.splice(cart.find(elem => elem.id() === id),1);
-}
-
 //Función para eliminar todos los productos del carrito
 
 function emptyCart() {
     cart.splice(0,cart.length);
-    let cartContainer = document.querySelector(".modal-body");
+    localStorage.clear();
+    let cartContainer = document.querySelector(".container-prods");
     cartContainer.innerHTML = "";
 }
 
@@ -76,29 +97,28 @@ function calculateTotalCart() {
     return total;
 }
 
+//Función para informar el total de la compra y vaciar el carrito
+
 function informTotal() {
-    swal("Your total price is U$D" + calculateTotalCart());
+    swal("The total price is U$D " + calculateTotalCart());
+    emptyCart();
 }
 
-// Función para renderizar los productos al sumarse al carrito
+// Función para renderizar un producto que se suma al carrito
 
 function renderCart(product) {
-    let sectionCart = document.querySelector(".modal-body");
-    let article = document.createElement("article");
-    article.innerHTML = `
-    <article class="m-2 cart-card" id=${product.id}>
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">${product.name}</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <p class="card-text">Price: U$D${product.price}</p>
-            </div>
-        </div>
-    </article>`;
-    sectionCart.appendChild(article);
+    let sectionCart = document.querySelector(".container-prods");
+    let row = document.createElement("div");
+    row.setAttribute("class", "row mt-2 d-flex justify-content-around align-items-center");
+    row.setAttribute("id", product.id);
+    row.innerHTML = `
+    <div class="col-12 d-flex align-items-center">
+        <p class="mb-0">${product.name} - U$D${product.price}</p>
+    </div>`;
+    sectionCart.appendChild(row);
 }
 
-// Función que suma el producto al carrito (evaluando si hay stock e informando el resultado de la operación) y renderiza el mismo en el HTML
+// Función que suma el producto al carrito (evaluando si hay stock e informando el resultado de la operación)
 
 function addToCart (event) {
     let idToFind = event.target.id;
@@ -106,13 +126,15 @@ function addToCart (event) {
     if (productFound.stock > 0) {
         cart.push(productFound);
         renderCart(productFound);
-        swal("Your product has been added to the cart!", "", "success",);
+        saveLocal("productsCart", JSON.stringify(cart));
+        swal("The product has been added to the cart!", "", "success",);
     }
     else {
         swal("The product is not available", "", "error",);
     }
-    
 }
+
+//Función para validar el formulario chequeando los campos ingresados. Envía por AJAX la información y retorna si la comunicación fue exitosa
 
 function validateForm (event) {
     event.preventDefault();
@@ -124,8 +146,8 @@ function validateForm (event) {
     let termsCond = children[8].firstElementChild.checked;
     let valid = true;
     if ((name === null || name === "") || (lastName === null || lastName === "") || (email === null || email === "") || (message === null || message === "") || (!termsCond)) {
-        swal("Please complete your name, last name, email, message and accept T&C","", "error");
         valid = false;
+        swal("Please complete your name, last name, email, message and accept T&C","", "error");
     }
     if (valid) {
         $.ajax({
@@ -137,31 +159,65 @@ function validateForm (event) {
                 formMssg: message,
             }),
             success: function (result) {
-                swal("Your message has been sent!", "", "success")
-                console.log(result)
+                swal("Your message has been sent!", "", "success");
+                console.log(result);
             },
             error: function (jqXHR) {
-                swal("Something went wrong, please try again in a few minutes", "", "error")
+                swal("Something went wrong, please try again in a few minutes", "", "error");
                 console.log(jqXHR);
             }
         })
     }
 }
 
-function resizeCart () {
-    $(".cart-icon").css({"font-size":"2.2rem"});
+//Función para renderizar los productos guardados en el arreglo de productos al ingresar a la página
+
+function renderContent() {
+    let container = document.querySelector(".products-container");
+    products.forEach(product => {
+        let item = document.createElement("article");
+        item.setAttribute("class", "m-3");
+        item.setAttribute("id", product.id);
+        item.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <img src="https://picsum.photos/300" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <h6>${product.author}</h6>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut neque diam, vulputate eget hendrerit vel, ultrices ac libero.</p>
+                <button href="#" id="${product.id}" class="btn btn-primary btn-buy">Add to cart</button>
+            </div>
+        </div>
+        `;
+    container.appendChild(item);
+    })
 }
 
-//Variables y constantes
+//Función para renderizar en el carrito los productos guardados en Localstorage (si los hubiese) al ingresar a la página
 
-const btnAddToCart = document.querySelectorAll(".btn-buy");
-const btnBuyAll = document.querySelector(".btn-buyall");
-const btnEmptyCart = document.querySelector(".btn-emptycart");
-const contactForm = $(".contact-form");
+function getFromStorage () {
+    let savedProducts = JSON.parse(localStorage.getItem("productsCart"));
+    if (savedProducts != null) {
+        let sectionCart = document.querySelector(".container-prods");
+        savedProducts.forEach (product => {
+            let row = document.createElement("div");
+            row.setAttribute("class", "row mt-2 d-flex justify-content-around align-items-center");
+            row.setAttribute("id", product.id);
+            row.innerHTML = `
+            <div class="col-12 d-flex align-items-center">
+                <p class="mb-0">${product.name} - U$D${product.price}</p>
+            </div>`;
+            sectionCart.appendChild(row);
+        })
+    }
+}
 
-//Listeners
+window.onload = () => {
 
-btnAddToCart.forEach(btn => btn.addEventListener('click', addToCart));
-btnEmptyCart.addEventListener('click', emptyCart);
-btnBuyAll.addEventListener('click', informTotal);
-contactForm.submit(validateForm);
+    renderContent();
+
+    getFromStorage();
+
+    const btnAddToCart = document.querySelectorAll(".btn-buy"); //Botones para añadir al carrito
+    btnAddToCart.forEach(btn => btn.addEventListener('click', addToCart)); //Evento para todos los botones de añadir al carrito
+}
